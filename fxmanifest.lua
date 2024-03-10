1aecf70f86e082df636a977a573f2628db1819ca
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-10 14:10:45 -0500

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'QBCore Command Scripts'
+version '1.0.0'
+
+client_scripts {
+    'mask.lua',
+    'hat.lua',
+    'emote.lua'
+}
+
+dependencies {
+    'qb-core'
+}
